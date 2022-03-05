b4c12b94bd46e50d79ebedd9fa730b61810c8cde
tmdgh1592 <tmdgh1592@naver.com>
2022-03-06 02:15:32 +0900

    - 영상통화, 푸시 디코딩

--- app/src/main/java/com/unithon/somethingnew/data/network/response/CallableFriendModel.kt
+++ app/src/main/java/com/unithon/somethingnew/data/network/response/CallableFriendModel.kt
@@ -3,18 +3,24 @@ package com.unithon.somethingnew.data.network.response
 import com.google.gson.annotations.SerializedName
 
 data class CallableFriendModel(
-    @SerializedName("uid")
+    @SerializedName("canCall")
+    val canCall: Boolean,
+    @SerializedName("email")
+    val email: String,
+    @SerializedName("imgUrl")
+    val profileUrl: String,
+    @SerializedName("street")
+    val street: String,
+    @SerializedName("userId")
     val uid: Long,
     @SerializedName("username")
     val username: String,
-    @SerializedName("imgUrl")
-    val profileUrl: String,
     @SerializedName("x")
     val x: Double,
     @SerializedName("y")
     val y: Double,
-    @SerializedName("fcmToken")
-    val fcmToken: String,
+
+
     /*@SerializedName("bellCount")
     val bellCount: Int,*/
 )
